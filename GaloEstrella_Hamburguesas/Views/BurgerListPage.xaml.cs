59a142748bd1b0124bf8903682cd0bd1d11379bc
Galo Andres Estrella Jimenez <galo.estrella.jimenez@udla.edu.ec>
2023-01-16 22:27:10 -0500

    .

--- GaloEstrella_Hamburguesas/Views/BurgerListPage.xaml.cs
+++ GaloEstrella_Hamburguesas/Views/BurgerListPage.xaml.cs
@@ -21,4 +21,14 @@ public partial class BurgerListPage : ContentPage
         burgerList.ItemsSource = burger;
     }
 
+    private async void SelectedItem(object sender,SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count != 0)
+        {
+            var burger=(Models.Burger)e.CurrentSelection[0];
+            await Shell.Current.GoToAsync($"{nameof(BurgerItemPage)}?{nameof(BurgerItemPage.Id)}={burger.Id}");
+            burgerList.SelectedItem = null;
+        }
+    }
+
 }
